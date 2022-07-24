bd947e11d80a1ff60e48180da2e3dfff374601ec
Jonas Pikiotas <j.pikiotas@gmail.com>
2022-07-24 14:13:22 +0100

    Add Header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,9 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Hulu!</h1>
-
       {/* Header  */}
+      <Header />
+
+      {/* Navigation  */}
+
+      {/* Results  */}
     </div>
   );
 }
