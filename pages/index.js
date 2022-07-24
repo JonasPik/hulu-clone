711b5d1c6c6b5533e3d6634c2bbae0fa10c8b75d
Jonas Pikiotas <j.pikiotas@gmail.com>
2022-07-24 17:10:45 +0100

    Add Navigation component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 
 export default function Home() {
   return (
@@ -11,10 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header  */}
       <Header />
-
-      {/* Navigation  */}
+      <Navigation />
 
       {/* Results  */}
     </div>
